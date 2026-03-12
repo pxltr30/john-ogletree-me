80cb74ea741147a37aeed736425d8660973fd444
John Ogletree <pxltr3@gmail.com>
2026-03-12 12:13:34 -0400

    Delete assets directory

--- assets/js/gtag-init.js
+++
@@ -1,9 +0,0 @@
-/**
- * Google Tag Manager / Analytics Initialization
- */
-window.dataLayer = window.dataLayer || [];
-function gtag(){
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-FY6WZRNX8M');
